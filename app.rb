993c156713674d17bb376d9df174513c9922ad26
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-10-30 18:56:19 +0100

    redirecting to /all to list all recipes

--- app.rb
+++ app.rb
@@ -2,11 +2,26 @@ require "sinatra"
 require "sinatra/reloader" if development?
 require "pry-byebug"
 require "better_errors"
+
+require_relative 'lib/cookbook'
+require_relative 'lib/controller'
+
+csv_file   = File.join(__dir__, 'lib/recipes.csv')
+cookbook   = Cookbook.new(csv_file)
+controller = Controller.new(@cookbook)
+
 configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
 
 get '/' do
-  'Welcome to the Cookbook!'
+  erb :index
 end
+
+get '/all' do
+  @recipes = cookbook.all
+  erb :all
+end
+
+
